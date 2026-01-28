04b9008c5a91eeedd7ae96baf0a2af9d456f3f56
AYUSH RAWAT <ayushrawat2007124@gmail.com>
2026-01-28 12:12:03 +0530

    Add files via upload

---
+++ Q8.py
@@ -0,0 +1,7 @@
+a=float(input("enter the value of a:"))
+b=float(input("enter the value of b :"))
+c=float(input("enter the value of c :"))
+s=(a+b+c)/2
+print(s)
+area=(s*(s-a)*(s-b)*(s-c))**0.5
+print(area)
