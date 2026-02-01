c34da828d0cc7c1260cacbe882a5a22b452ef180
AYUSH RAWAT <ayushrawat2007124@gmail.com>
2026-02-01 13:28:35 +0530

    Add files via upload

---
+++ Q12.py
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("A B | A&B A|B A^B\n");
+    printf("-----------------\n");
+
+    for (a = 0; a <= 1; a++) {
+        for (b = 0; b <= 1; b++) {
+            printf("%d %d |  %d   %d   %d\n", a, b, a & b, a | b, a ^ b);
+        }
+    }
+
+    return 0;
+}
