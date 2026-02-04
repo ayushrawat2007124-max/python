93870990ae5928603878d2e59466e81794e76ea5
AYUSH RAWAT <ayushrawat2007124@gmail.com>
2026-02-04 15:55:00 +0530

    Q14.py

---
+++ Q14.py
@@ -0,0 +1,7 @@
+seq = (10, 20, 56, 78, 89)
+num = int(input("Enter a number: "))
+
+if num in seq:
+    print("Number is present in the sequence")
+else:
+    print("Number is not present in the sequence")
