e9e146290bf77f58b0c57c2a7ce237dea5b69f90
AYUSH RAWAT <ayushrawat2007124@gmail.com>
2026-02-26 20:41:16 +0530

    Add files via upload

---
+++ Q37.py
@@ -0,0 +1,10 @@
+str=input("enter a  string:")
+sub=input("enter a substring:")
+word=str.split()
+count=0
+start=0
+while True:
+    pos=str.find(sub,start)
+    count=count+1
+print(count)        
+
