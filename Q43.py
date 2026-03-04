cd4475a1b669ddd6a1bbbfa868e4ab0386ae1d1c
AYUSH RAWAT <ayushrawat2007124@gmail.com>
2026-03-04 22:34:24 +0530

    Add files via upload

---
+++ Q43.py
@@ -0,0 +1,15 @@
+'''
+8.	Take two sets and apply various set operations on them :
+S1 = {Red ,yellow, orange , blue }
+S2 = {violet, blue , purple}
+'''
+
+S1 = {"Red", "yellow", "orange", "blue"}
+S2 = {"violet", "blue", "purple"}
+print("Union:", S1 | S2)
+print("Intersection:", S1 & S2)
+print("Difference (S1 - S2):", S1 - S2)
+print("Difference (S2 - S1):", S2 - S1)
+print("Symmetric Difference:", S1 ^ S2)
+print("Is S1 subset of S2?", S1.issubset(S2))
+print("Is S2 subset of S1?", S2.issubset(S1))
