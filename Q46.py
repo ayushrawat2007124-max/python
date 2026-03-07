77e1a1395b6f530a538d86924c842185bbdc31bc
AYUSH RAWAT <ayushrawat2007124@gmail.com>
2026-03-07 21:31:30 +0530

    Add files via upload

---
+++ Q46.py
@@ -0,0 +1,15 @@
+#3.	Write a Python function to print 1 to n using recursion. (Note: Do not use loop)
+def print_numbers(n):
+   
+    if n <= 0:
+        return
+    
+    
+    print_numbers(n - 1)
+    
+    
+    print(n)
+
+
+
+print_numbers(5)
