1d09c3bd657c5a2993096791816dfbe8c26ad99c
AYUSH RAWAT <ayushrawat2007124@gmail.com>
2026-03-09 17:54:51 +0530

    Add files via upload

---
+++ Q48.py
@@ -0,0 +1,9 @@
+#5.	Write a lambda function to find volume of cone.
+
+volume_of_cone = lambda r, h: (1/3) * 3.14159 * r * r * h
+
+
+radius = 3
+height = 7
+
+print("Volume of cone:", volume_of_cone(radius, height))
