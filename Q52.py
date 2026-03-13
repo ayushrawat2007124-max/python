f66c4215c70efa787a95b0046f80e54578e9f28d
AYUSH RAWAT <ayushrawat2007124@gmail.com>
2026-03-13 21:37:57 +0530

    Add files via upload

---
+++ Q52.py
@@ -0,0 +1,11 @@
+#9.	Write a program to create two lists and generate a dictionary with keys from list1 and values from list2.
+
+list1 = ['a', 'b', 'c', 'd']
+list2 = [10, 20, 30, 40]
+
+
+result_dict = dict(zip(list1, list2))
+
+
+
+print(result_dict   )
