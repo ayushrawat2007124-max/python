ef037827ac25ae5601354efbe4b971263c1762ad
AYUSH RAWAT <ayushrawat2007124@gmail.com>
2026-04-28 10:47:33 +0530

    Add files via upload

---
+++ Q77.py
@@ -0,0 +1,12 @@
+#1.	Create numpy array to find sum of all elements in an array.
+import numpy as np
+
+# Create a NumPy array
+arr = np.array([10, 20, 30, 40, 50])
+
+# Find sum of all elements
+total_sum = np.sum(arr)
+
+# Display result
+print("Array:", arr)
+print("Sum of all elements:", total_sum)
